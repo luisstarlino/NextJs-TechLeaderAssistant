78efd147ade2bd6402065add116fd6905f1fb224
Luis Guilherme Soares Starlino <luis.starlino@mgs.srv.br>
2025-10-22 18:41:42 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,16 +2,7 @@
 import { initializeApp, getApp, getApps } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
-
-// It's recommended to use environment variables for this in a real project.
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-};
+import { firebaseConfig } from '@/firebase/config';
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
@@ -19,4 +10,3 @@ const db = getFirestore(app);
 const auth = getAuth(app);
 
 export { app, db, auth };
-
