c6868a1315d5a28583fb1d3bec0492f53e22c2c9
Luis Guilherme Soares Starlino <luis.starlino@mgs.srv.br>
2025-10-22 18:41:26 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1757223725-be973",
+  "appId": "1:454072834813:web:95e80913a5be7f9e1dbc94",
+  "apiKey": "AIzaSyAhtsvjGp1pWflG4MjillJMkGd5KcEAxqA",
+  "authDomain": "studio-1757223725-be973.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "454072834813"
+};
