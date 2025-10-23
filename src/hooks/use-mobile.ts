7ec6bdf25406aea211f05516d774cd1a82617b86
Luis Guilherme Soares Starlino <luis.starlino@mgs.srv.br>
2025-10-23 22:46:43 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@/

---
+++ src/hooks/use-mobile.ts
@@ -0,0 +1,21 @@
+'use client';
+
+import * as React from 'react';
+
+export function useIsMobile() {
+  const [isMobile, setIsMobile] = React.useState(false);
+
+  React.useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 768px)');
+    setIsMobile(mediaQuery.matches);
+
+    const handler = (event: MediaQueryListEvent) => {
+      setIsMobile(event.matches);
+    };
+
+    mediaQuery.addEventListener('change', handler);
+    return () => mediaQuery.removeEventListener('change', handler);
+  }, []);
+
+  return isMobile;
+}
